0384fe26626fb6ca8e4dfc7f436158bed5a438f2
l19328 <47017295+l19328@users.noreply.github.com>
2019-03-08 10:38:25 +0800

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -2,8 +2,8 @@
 #TODO: add build script
 
 set -x
-ln -s /home/travis/gopath/src/github.com/h3copenr/comwaresdk /home/travis/gopath/src/github.com/h3copen/comwaresdk
-ln -s /home/travis/gopath/src/github.com/h3copenr/h3cfibservice /home/travis/gopath/src/github.com/h3copen/h3cfibservice
+ln -s /home/travis/gopath/src/github.com/h3copen/h3copenr/comwaresdk /home/travis/gopath/src/github.com/h3copen/comwaresdk
+ln -s /home/travis/gopath/src/github.com/h3copen/h3copenr/h3cfibservice /home/travis/gopath/src/github.com/h3copen/h3cfibservice
 cd /home/travis/gopath/src/github.com/h3copen
 cd /home/travis/gopath/src/github.com/h3copen/h3cfibservice/fibhandler
 go build

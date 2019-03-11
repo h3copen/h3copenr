d948939abfbba29f3f13dcd17833a54fd9934d00
l19144 <47175209+l19144@users.noreply.github.com>
2019-03-11 16:33:21 +0800

    Update test.sh

--- build/test.sh
+++ build/test.sh
@@ -55,14 +55,15 @@ function cmp_routes()
 #set -e
 docker pull lmke/h3c_openr:v5
 docker run -itd --name complie_openr lmke/h3c_openr:v5  bash
-docker exec -it complie_openr sh -c " git clone https://github.com/facebook/openr.git && cd /openr/build && ./build_openr.sh"
+docker exec -it complie_openr sh -c " git clone https://github.com/facebook/openr.git && cd /openr/build "
 if [ $? -eq 0 ]; then 
-	s=`docker exec complie_openr sh -c "cd /openr && git log | head -1" `
+	s=`docker exec -t complie_openr sh -c "cd /openr && git log | head -1" `
 	#echo $s
 	#file commit.id.log
 	#echo `pwd`
 	#echo `pwd`/commit.id.log
 	echo $s >> `pwd`/commit.id.log
+	echo $s >> commit.id.log 
 	#docker cp complie_openr:/usr/local/sbin/openr . 
 	#touch openr
 	docker build -f dockerfile_openr -t openr:test .
